b32193bd991b6aa315f5c6c9ac74974a6603b07d
Milly <milabits@gmail.com>
2021-01-11 13:29:35 +0100

    Added Textmeshpro, fish now have names and can be inspected

---
+++ Ctouch Aquarium/Assets/Scripts/FishData/Fish.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace FishData
+{
+    public class Fish : MonoBehaviour
+    {
+        [SerializeField] private string fishName;
+
+        public string GetName() => fishName;
+
+        private void OnMouseDown() => ShowTooltip();
+
+        private void ShowTooltip()
+        {
+            FishUI ui = FindObjectOfType<FishUI>();
+            ui.gameObject.SetActive(true);
+            ui.Init(this);
+        }
+    }
+}
---
+++ Ctouch Aquarium/Assets/Scripts/FishData/FishUI.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FishData
+{
+    public class FishUI : MonoBehaviour
+    {
+        [SerializeField] private Image bubble;
+        [SerializeField] private TextMeshProUGUI nameText;
+
+        private Vector3 defaultPos;
+
+        private Fish fish;
+
+        private void Awake()
+        {
+            defaultPos = transform.position;
+            nameText = GetComponentInChildren<TextMeshProUGUI>();
+        }
+
+        private void Update()
+        {
+            if (transform.position != defaultPos)
+                transform.position = fish.transform.position;
+        }
+
+        public void Init(Fish fish)
+        {
+            this.fish = fish;
+            nameText.text = fish.GetName();
+
+            Vector3 boundSize = fish.GetComponentInChildren<SkinnedMeshRenderer>().bounds.size;
+            Debug.Log(boundSize);
+            float size = Mathf.Max(boundSize.x, boundSize.y);
+            size = Mathf.Max(boundSize.z, size);
+
+            bubble.transform.localScale = Vector3.one * size;
+            transform.position = fish.transform.position;
+        }
+    }
+}
