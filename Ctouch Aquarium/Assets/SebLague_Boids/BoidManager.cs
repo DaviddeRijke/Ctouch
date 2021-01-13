af71aedaa4e2264e036f56d97a494af55553de6d
Kelvin <kelvinvanboekel@gmail.com>
2021-01-13 11:40:20 +0100

    fix boids crashing with 0 fish

--- Ctouch Aquarium/Assets/SebLague_Boids/BoidManager.cs
+++ Ctouch Aquarium/Assets/SebLague_Boids/BoidManager.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoidManager : MonoBehaviour {
+public class BoidManager : MonoBehaviour
+{
 
     const int threadGroupSize = 1024;
 
@@ -10,10 +11,12 @@ public class BoidManager : MonoBehaviour {
     //public ComputeShader compute;
     List<Boid> boids = new List<Boid>();
 
-    void Start () {
-        boids.AddRange(FindObjectsOfType<Boid> ());
-        foreach (Boid b in boids) {
-            b.Initialize (settings, null);
+    void Start()
+    {
+        boids.AddRange(FindObjectsOfType<Boid>());
+        foreach (Boid b in boids)
+        {
+            b.Initialize(settings, null);
         }
 
     }
@@ -38,44 +41,51 @@ public class BoidManager : MonoBehaviour {
         boids.Remove(fish.GetComponent<Boid>());
     }
 
-    void Update () {
-        if (boids != null) {
+    void Update()
+    {
+        if (boids != null)
+        {
+            if (boids.Count != 0)
+            {
+                int numBoids = boids.Count;
+                var boidData = new BoidData[numBoids];
 
-            int numBoids = boids.Count;
-            var boidData = new BoidData[numBoids];
+                for (int i = 0; i < boids.Count; i++)
+                {
+                    boidData[i].position = boids[i].position;
+                    boidData[i].direction = boids[i].forward;
+                }
 
-            for (int i = 0; i < boids.Count; i++) {
-                boidData[i].position = boids[i].position;
-                boidData[i].direction = boids[i].forward;
+                var boidBuffer = new ComputeBuffer(numBoids, BoidData.Size);
+                boidBuffer.SetData(boidData);
+
+                //compute.SetBuffer (0, "boids", boidBuffer);
+                //compute.SetInt ("numBoids", boids.Length);
+                //compute.SetFloat ("viewRadius", settings.perceptionRadius);
+                //compute.SetFloat ("avoidRadius", settings.avoidanceRadius);
+
+                int threadGroups = Mathf.CeilToInt(numBoids / (float)threadGroupSize);
+                //compute.Dispatch (0, threadGroups, 1, 1);
+
+                boidBuffer.GetData(boidData);
+
+                for (int i = 0; i < boids.Count; i++)
+                {
+                    boids[i].avgFlockHeading = boidData[i].flockHeading;
+                    boids[i].centreOfFlockmates = boidData[i].flockCentre;
+                    boids[i].avgAvoidanceHeading = boidData[i].avoidanceHeading;
+                    boids[i].numPerceivedFlockmates = boidData[i].numFlockmates;
+
+                    boids[i].UpdateBoid();
+                }
+
+                boidBuffer.Release();
             }
-
-            var boidBuffer = new ComputeBuffer (numBoids, BoidData.Size);
-            boidBuffer.SetData (boidData);
-
-            //compute.SetBuffer (0, "boids", boidBuffer);
-            //compute.SetInt ("numBoids", boids.Length);
-            //compute.SetFloat ("viewRadius", settings.perceptionRadius);
-            //compute.SetFloat ("avoidRadius", settings.avoidanceRadius);
-
-            int threadGroups = Mathf.CeilToInt (numBoids / (float) threadGroupSize);
-            //compute.Dispatch (0, threadGroups, 1, 1);
-
-            boidBuffer.GetData (boidData);
-
-            for (int i = 0; i < boids.Count; i++) {
-                boids[i].avgFlockHeading = boidData[i].flockHeading;
-                boids[i].centreOfFlockmates = boidData[i].flockCentre;
-                boids[i].avgAvoidanceHeading = boidData[i].avoidanceHeading;
-                boids[i].numPerceivedFlockmates = boidData[i].numFlockmates;
-
-                boids[i].UpdateBoid ();
-            }
-
-            boidBuffer.Release ();
         }
     }
 
-    public struct BoidData {
+    public struct BoidData
+    {
         public Vector3 position;
         public Vector3 direction;
 
@@ -84,9 +94,11 @@ public class BoidManager : MonoBehaviour {
         public Vector3 avoidanceHeading;
         public int numFlockmates;
 
-        public static int Size {
-            get {
-                return sizeof (float) * 3 * 5 + sizeof (int);
+        public static int Size
+        {
+            get
+            {
+                return sizeof(float) * 3 * 5 + sizeof(int);
             }
         }
     }
